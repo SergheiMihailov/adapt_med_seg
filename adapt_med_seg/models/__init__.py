118a91df9c6590899e2611d54c701c077feab569
Danilo Toapanta <70687643+danilotpnta@users.noreply.github.com>
2024-05-15 17:48:09 +0200

    add lora models to dict

--- adapt_med_seg/models/__init__.py
+++ adapt_med_seg/models/__init__.py
@@ -1,6 +1,8 @@
-from adapt_med_seg.models.segvol_base import SegVolBase
+from adapt_med_seg.models.segvol_base import  SegVolBase, SegVolLoraViT, SegVolLoraAll
 
 
 MODELS = {
     "segvol_baseline": SegVolBase,
+    "segvol_lora_vit": SegVolLoraViT,
+    "segvol_lora_all": SegVolLoraAll,
 }
