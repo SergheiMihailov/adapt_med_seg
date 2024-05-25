721eba6ecc9f63f4e19e615d7e8ae7aa47743602
MJHamar <hamiki987@gmail.com>
2024-05-25 14:02:46 +0200

    updated sh and job file to run AMOS and BRATS preprocessing

--- jobs/preprocess.sh
+++ jobs/preprocess.sh
@@ -6,18 +6,20 @@ cd preprocessing
 # CLI arguments
 
 # common
-CLASSES=("prostate")
-DATASET_ROOTS_ROOT='../datasets' # rel. to preprocessing
-SAVE_ROOTS_ROOT='../datasets/processed' # rel. to preprocessing
-NUM_WORKERS=4
+# --dataset_root /scratch-shared/scur0402/AMOS_2022 --dataset_code amos000 --save_root /scratch-shared/scur0402/ --num_workers 4 --dataset_type AMOS
+tch-shared/scur0402/ --num_workers 4 --dataset_type AMOS                                                           
+CLASSES=("liver", "right kidney", "spleen", "left kidney")
+DATASET_ROOTS_ROOT="/scratch-shared/scur0402" 
+SAVE_ROOTS_ROOT="/scratch-shared/scur0402"
+NUM_WORKERS=8
 SEED=42
 TEST_RATIO=0.2
 VAL_RATIO=0.2
 # dataset-specific
-DATASET_ROOTS=("MSD" "promise12" "SAML" "T2-weighted-MRI")
-DATASET_CODES=("MSD_mr_42" "promise12_mr_42" "SAML_mr_42" "T2-weighted-MRI_mr_42")
+DATASET_ROOTS=("AMOS2022" "BRATS2021")
+DATASET_CODES=("amos_mrct_42" "brats_mr_42")
 SAVE_ROOTS=$DATASET_ROOTS
-DATASET_TYPES=("MSD_Prostate" "PROMISE12" "SAML" "T2W-MRI")
+DATASET_TYPES=("AMOS" "BRATS")
 
 # iterate 4 times and preprocess each dataset
 # with the respective dataset-specific arguments
